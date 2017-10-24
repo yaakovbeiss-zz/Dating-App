c05bcd11a09658e20fa7d135c8d653fea9405ef4
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-10-24 12:49:03 -0400

    Setup React

---
+++ frontend/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Route,
+  Redirect,
+  Switch,
+  Link
+} from 'react-router-dom';
+
+const App = () => (
+
+  <div>
+    
+  </div>
+
+);
+
+export default App;
