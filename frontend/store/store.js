af0dfa4926beedc60e1e76fb3bfbd2313410308f
Yaakov Beiss <yaakovbeiss@gmail.com>
2017-10-29 11:26:34 -0400

    Add docs

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,11 +4,11 @@ import RootReducer from '../reducers/root_reducer';
 
 const middlewares = [thunk];
 
-// if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== 'production') {
 
   const { createLogger } = require('redux-logger');
   middlewares.push(createLogger());
-// }
+}
 const configureStore = (preloadedState = {}) => (
   createStore(
     RootReducer,
